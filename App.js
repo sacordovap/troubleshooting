a5d93c3175ce8fa2d888fc579af5e7d1738a68b7
Jhon Ccamerccoa Perez <jccamerccoa@unsa.edu.pe>
2021-10-11 17:58:36 -0500

    r

--- App.js
+++ App.js
@@ -26,20 +26,26 @@ function MyStack() {
     <Stack.Navigator screenOptions={{
       headerShown: false
     }}> 
-    <Stack.Screen name="Evento Causa"
+ <Stack.Screen name="Evento Causa"
         component={EventoCausa} /> 
- <Stack.Screen name="Login"
-        component={Login} />
 <Stack.Screen name="Resultado Conclusion"
         component={ResultadoConclusion} />
+<Stack.Screen name="Acciones Tomadas"
+        component={AccionesTomadasScreen} />
+<Stack.Screen name=" Datos Equipo"
+        component={DataEquipo} />
+
+
+   
+ <Stack.Screen name="Login"
+        component={Login} />
+
      
 <Stack.Screen name="Datos Iniciales"
         component={DatosInicialesScreen} />
  
-       <Stack.Screen name="Acciones Tomadas"
-        component={AccionesTomadasScreen} />
-    <Stack.Screen name=" Datos Equipo"
-        component={DataEquipo} />
+       
+    
 <Stack.Screen name="Home"
         component={HomeScreen}
         options={{ title: 'Home' }} />
--- components/AccionesTomadas.js
+++ components/AccionesTomadas.js
@@ -4,7 +4,7 @@ import { StyleSheet, View, Text, TextInput } from "react-native";
 function AccionesTomadas(props) {
   return (
     <View style={[styles.container, props.style]}>
-      <Text style={styles.acciones}>Acciones Tomadas</Text>
+      <Text style={styles.acciones}>Acciones Tomadas </Text>
       
     </View>
   );
